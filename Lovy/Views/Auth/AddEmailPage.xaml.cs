73c3eb1321a8a1f00b64a8c3899fcef3a8c8cddf
Esamai Emmanuel <Esamaiemmanuel@users.noreply.github.com>
2023-07-11 16:47:21 +0300

    .

--- Lovy/Views/Auth/AddEmailPage.xaml.cs
+++ Lovy/Views/Auth/AddEmailPage.xaml.cs
@@ -11,4 +11,13 @@ public partial class AddEmailPage : ContentPage
     {
          Shell.Current.GoToAsync($"//{nameof(LoginPage)}");
     }
+
+    private void btnSubmit_Clicked(object sender, EventArgs e)
+    {
+        string email = mailEntry.Text;
+
+        // Validate and process the email address
+        // For example, display a message or perform an action
+        DisplayAlert("Submitted", $"Email: {email}", "OK");
+    }
 }
